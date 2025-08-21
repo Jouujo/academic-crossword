07a530a2bfa2e97bca54ddb6182e9a8d8aa66298
Joanna Chimalilo <115937289+Jouujo@users.noreply.github.com>
2025-08-21 13:22:20 -0500

    Update next.config.ts

--- academic-crossword/next.config.ts
+++ academic-crossword/next.config.ts
@@ -1,15 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // ✅ Allow production builds to succeed even if there are ESLint errors.
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // allow build even if lint errors
   },
 };
 
